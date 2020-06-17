35f02a508fefe2d903b2e1e2f8fc5330ede905ba
AVData <AVData@users.noreply.github.com>
2020-06-17 14:02:58 -0700

    completed day 2 mvp

--- ls8/ls8.py
+++ ls8/ls8.py
@@ -4,8 +4,19 @@
 
 import sys
 from cpu import *
+file = open(sys.argv[1], 'r')
+
+program = []
+
+for line in file:
+    line = line.split(' ')[0]
+    line = line.split('#')[0]
+    if len(line) >= 8:
+        program.append(int(line, 2))
+
+flie.close()
 
 cpu = CPU()
 
-cpu.load()
+cpu.load(program)
 cpu.run()
